998b89e19a495256d72047afb2fcf3a0a9d1e27d
Sohel <sohel@simformsolutions.com>
2023-05-16 15:00:44 +0530

    Refact

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const inquirer = require("inquirer");
 const app = expres();
 const validation = require("./util/validationTime");
 const convert=require('./util/convertTime')
+const moment=require('moment')
+console.log(moment().subtract(10, 'days').calendar());
+
 inquirer
   .prompt([
     {
@@ -19,6 +22,7 @@ inquirer
       type: "input",
       name: "currentTime",
       message: "CURRENT_TIME : ",
+      default:moment().format('LT')
     },
   ])
   .then((answer) => {
@@ -26,9 +30,11 @@ inquirer
     let CONVERT_TO_TIMEZONE = answer.convertToZone;
     let CURRENT_TIME = answer.currentTime;
 
+    // validate the Date
     if (!validation(CURRENT_TIME)) {
       throw new Error("PLease Enter Valid Time");
     }
+    // call convert function to covert time
     const ConvertedTime=convert(CURRENT_TIMEZONE,CONVERT_TO_TIMEZONE,CURRENT_TIME)
     console.log(ConvertedTime);
   })
--- util/convertTime.js
+++ util/convertTime.js
@@ -1,84 +1,21 @@
 const timeZones = require("../data/timezones.json");
 
 const convert = (currentTimeZone, convertToZone, currentTime) => {
-//   let hours = currentTime.split(":")[0];
-//   let min = currentTime.split(":")[1];
-//   let currentMilisec=(hours*3600000)+(min*60000);
-//   console.log(currentMilisec*5.3);
+
   let currentOffset = timeZones[currentTimeZone]["offset"];
   let convertOffset = timeZones[convertToZone]["offset"];
   let totalOffset = convertOffset-currentOffset;
-   // convert the current time to a date object
+
+   // create Dummy date
    const date = new Date(`01/01/2000 ${currentTime}`);
    const time_diff = totalOffset * 60 * 60 * 1000;
 
-   // add the time difference in the old time and form the date
+   // add the time difference in the old time and f the date
    date.setTime(date.getTime() + time_diff);
 
-   // convert the result back to a string in the desired format
+   // convert the retult of above into date
    const converted_time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric'});
    return converted_time;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   console.log(totalOffset*3600000);
-//   var utc = currentMilisec + (currentOffset *60*60000);
-// //   var nd = new Date(utc + 3600000 * );
-//   console.log(utc);
-//   return utc;
-
-  const NewTimezone = (current_time, total_offset) => {
-    
-    // convert the current time to a date object
-    const date = new Date(`01/10/2010 ${current_time}`);
-    const time_diff = total_offset * 60 * 60 * 1000;
-
-    // add the time difference in the old time and form the date
-    date.setTime(date.getTime() + time_diff);
-
-    // convert the result back to a string in the desired format
-    const converted_time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric'});
-    return converted_time;
-}
-
-module.exports = NewTimezone;
-
-  console.log(utc);
-  console.log(nd.toLocaleString());
-  console.log(totalOffset);
-  //   var b = new Date();
-  //   console.log("local  time", b.toLocaleString());
-  //   console.log(hours);
-  //   console.log(currentMilisec);
-  //   var utc = currentMilisec + (currentOffset*60) * 60000;
-  //   console.log("UTC",utc);
-  //   var nd = new Date(utc + (3600000 * (-convertOffset*60)));
-  //   console.log(nd);
-  //   //   console.log(nd.toLocaleString());
-  //   return nd;
 };
 module.exports = convert;
